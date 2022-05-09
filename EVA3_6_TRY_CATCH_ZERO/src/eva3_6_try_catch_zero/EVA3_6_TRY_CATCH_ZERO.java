7c1afaa4a8820ec43ad2dc8fdfb1ffd4663f012f
Azul Siret Cordero <101304946+Liar17@users.noreply.github.com>
2022-05-09 09:50:02 -0600

    Add files via upload

---
+++ EVA3_6_TRY_CATCH_ZERO/src/eva3_6_try_catch_zero/EVA3_6_TRY_CATCH_ZERO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_6_try_catch_zero;
+import java.util.Scanner;
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_6_TRY_CATCH_ZERO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("introduce un numero entero 1:");
+        int entero = input.nextInt();
+        System.out.println("Introduce un numero entero 2:");
+        int entero2 = input.nextInt();
+        //------------
+        try{
+            System.out.println("Division: " + (entero/entero2));
+        }catch(ArithmeticException e){
+            //e.printStackTrace(); //imprime la excepcion sin que el programa falle
+            System.out.println("Se produjo una division entre cero");
+        }
+        System.out.println("Termina el programa");
+    }
+    
+}
